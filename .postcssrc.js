72ca1de342ef8b8b6c20a44608d557a6ae73efa6
Brandon Blaylock <brandon@null.pub>
2018-11-11 18:44:01 -0800

    Initial Commit
    First pass at a super slim postcss utilities based css library.

---
+++ .postcssrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  plugins: [
+    require('postcss-import')(),
+    require('postcss-for')(),
+    require('postcss-each')(),
+    require('postcss-simple-vars')(),
+    require('postcss-cssnext')({
+      browsers: ['last 2 versions'],
+      features: {
+        customProperties: {
+          preserve: true
+        }
+      }
+    }),
+    require('postcss-style-guide')({
+      project: 'nll css',
+      dest: 'docs/index.html',
+      showCode: false
+    }),
+    require('cssnano')()
+  ]
+};
---
+++ README.md
@@ -0,0 +1,20 @@
+# Minimal CSS Utility Classes
+
+This is a collection of postcss processable css classes that cover the following properties
+
+- Colors
+- Borders
+- Margins
+- Paddings
+- Flexbox
+
+The goal was to create bootstrap-like utility classes with a more standard naming scheme and a heavy reliance on css variables.
+
+### To Use
+
+_Documentation Needed_
+
+### To Do
+
+- Add comments that the documentation generator can pick up.
+- Finalize patterns for flexbox, borders, etc.
